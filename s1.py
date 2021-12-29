d3c6fb667c1d5ca23804389765420116c91f3781
Shashi1408 <47396633+Shashi1408@users.noreply.github.com>
2021-12-29 03:09:04 -0800

    Update s1.py

--- s1.py
+++ s1.py
@@ -4,18 +4,3 @@ print("Monkey is dancing")
 
 print("Monkey1 is dancing")
 print("Monkey1 is dancing")
-
-
-print("Donkey is dancing")
-print("Donkey is dancing")
-
-print("Monkey is dancing")
-print("Monkey is dancing")
-
-
-print("Monkey1 is dancing")
-print("Monkey1 is dancing")
-
-
-print("Donkey is dancing")
-print("Donkey is dancing")
