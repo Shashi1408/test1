98ff6acaec61693f6a0b3037d86fb0b97f13b96e
Shashi1408 <sdhar.r.14@gmail.com>
2024-02-18 18:28:15 +0000

    23rd commit on 17th Feb

--- s1.py
+++ s1.py
@@ -7,9 +7,6 @@ print("Monkey1 is dancing")
 print("Monkey2 is dancing")
 print("Monkey2 is dancing")
 
-print("Monkey3 is dancing")
-print("Monkey3 is dancing")
-
 print("Donkey is dancing")
 print("Donkey is dancing")
 
@@ -19,5 +16,3 @@ print("Donkey1 is dancing")
 print("Donkey2 is dancing")
 print("Donkey2 is dancing")
 
-print("Donkey3 is dancing")
-print("Donkey3 is dancing")
