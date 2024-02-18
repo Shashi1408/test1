8fc24b2b18394a2bc073db65663730f55b79c5f7
Shashi1408 <sdhar.r.14@gmail.com>
2024-02-18 08:19:03 +0000

    12th commit on 17th Feb

--- s2.py
+++ s2.py
@@ -4,10 +4,6 @@ print("Monkey is dancing")
 print("Monkey1 is dancing")
 print("Monkey1 is dancing")
 
-
-print("Monkey2 is dancing")
-print("Monkey2 is dancing")
-
 print("Donkey is dancing")
 print("Donkey is dancing")
 
