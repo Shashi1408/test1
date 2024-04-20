af2598e16073e295c4b10d1c4857695e819fa5d1
root <root@ip-172-31-21-190.me-central-1.compute.internal>
2024-04-20 16:28:21 +0000

    1st commit on Apr 20

--- s1.py
+++ s1.py
@@ -4,6 +4,9 @@ print("Monkey is dancing")
 print("Monkey1 is dancing")
 print("Monkey1 is dancing")
 
+print("Monkey1 is dancing")
+print("Monkey1 is dancing")
+
 print("Donkey is dancing")
 print("Donkey is dancing")
 
