056e73048a3a3058e024ed540d9bbf4bfbec4a8f
root <root@ip-172-31-26-201.me-central-1.compute.internal>
2024-04-22 16:48:20 +0000

    2nd commit on 04222024

--- s2.py
+++ s2.py
@@ -4,12 +4,8 @@ print("Monkey is dancing")
 print("Monkey1 is dancing")
 print("Monkey1 is dancing")
 
-print("Monkey2 is dancing")
-print("Monkey2 is dancing")
-
 print("Donkey is dancing")
 print("Donkey is dancing")
 
 print("Donkey1 is dancing")
 print("Donkey1 is dancing")
-
