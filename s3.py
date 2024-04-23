bbbace80d8bb3beb4f272b6df312670adf498348
root <root@ip-172-31-16-113.me-central-1.compute.internal>
2024-04-23 16:55:13 +0000

    10th commit on 04232024

--- s3.py
+++ s3.py
@@ -6,12 +6,3 @@ print("Monkey1 is dancing")
 
 print("Monkey2 is dancing")
 print("Monkey2 is dancing")
-
-print("Donkey is dancing")
-print("Donkey is dancing")
-
-print("Donkey1 is dancing")
-print("Donkey1 is dancing")
-
-print("Donkey2 is dancing")
-print("Donkey2 is dancing")
